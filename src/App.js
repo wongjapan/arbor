2a3ec042457f5abf04e2dd860a41f79c1b6a43a9
samshals19 <56256306+samshals19@users.noreply.github.com>
2022-08-25 23:42:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import App1 from "./App1";
+import App2 from "./App2";
 
 function App() {
   return (
     <>
       <div className="App">
         <App1 />
+        {/* <App2 /> */}
       </div>
     </>
   );
